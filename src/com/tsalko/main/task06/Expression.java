14ab0ec765584612603db0604df0304796589578
IgorTsalko <tracer.pk@mail.ru>
2020-06-08 12:13:27 +0300

    Refactoring

--- src/com/tsalko/main/task06/Expression.java
+++ src/com/tsalko/main/task06/Expression.java
@@ -9,55 +9,37 @@ public class Expression {
         long n;
 
         // random values
-        a1 = 100_000_000;
-        d = -29_000_000;
+        a1 = 1000000;
+        d = -1000;
 
-        n = findCountForInt(a1, d);
+        n = findStepForInt(a1, d);
         System.out.println("Count for int: " + n);
 
-
-        n = findCountForLong(a1, d);
+        n = findStepForLong(a1, d);
         System.out.println("Count for long: " + n);
     }
 
-    /*
-    Приходиться писать такую сложную логику проверки, т.к. если а1 и d разнознаковые,
-    сумма будет сначала увеличиваться, а после уменьшаться.
-    Пример суммы на каждом шаге:
-
-    Sum: 100000000
-    Sum: 171000000
-    Sum: 213000000
-    Sum: 226000000
-    Sum: 210000000
-    Sum: 165000000
-    Sum: 91000000
-    Sum: -12000000
-    Sum: -144000000
-    Sum: -305000000
-    Sum: -495000000
-    Sum: -714000000
-    Sum: -962000000
-    Sum: -1239000000
-    Sum: -1545000000
-    Sum: -1880000000
-
-    Так же проблемы при:
-    a1 = Integer.MAX_VALUE;
-    d = Integer.MAX_VALUE;
-    которые обходяться доп. блоком if
+    /**
+     * Приходиться писать сложную логику проверки, т.к. есть большо количество
+     * начальных значений a1 и d; Разнознаковые, с одинаковым знаком,
+     * близкое значение к максимальному и т.д и т.п.
+     *
+     * Метод протестирован на все возможные вариации, которые удалось вспомнить
+     *
+     * @param a1 начальное а
+     * @param d шаг функции
+     * @return шаг, на котором функция превысит значение int
      */
+    private int findStepForInt(int a1, int d) {
 
-    private int findCountForInt(int a1, int d) {
-        // Название переменных взято из формулы в условии
-
-        // Начальное сумма равна 0
-        int sN = 0;
-        // Отсчет от 1-го n
-        int n = 1;
         int aN;
-        int aNPrevious = 0;
+        int sN;
+        int n;
+        int aNPrevious;
 
+        sN = 0;
+        n = 1;
+        aNPrevious = 0;
         while (true) {
             aN = a1 + ((n - 1) * d);
 
@@ -82,15 +64,16 @@ public class Expression {
         return n;
     }
 
-    private long findCountForLong(long a1, long d) {
-        // Название переменных взято из формулы в условии
-        // Начальное сумма равна 0
-        long sN = 0;
-        // Отсчет от 1-го n
-        long n = 1;
-        long aN;
-        long aNPrevious = 0;
+    private long findStepForLong(long a1, long d) {
 
+        long aN;
+        long sN;
+        long n;
+        long aNPrevious;
+
+        sN = 0;
+        n = 1;
+        aNPrevious = 0;
         while (true) {
             aN = a1 + (n - 1) * d;
 
